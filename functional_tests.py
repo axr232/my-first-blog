79c7f34e8ed3a117f8caac1831682b781939b8d6
Alexandru Radut <18radut@gmail.com>
2020-08-28 06:03:43 +0300

    working on tests

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,14 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
-        
+
+    def check_if_all_forms_present(self):
+        cv_profile = self.browser.find_element_by_id('cv_profile').text
+        cv_education = self.browser.find_element_by_id('cv_education').text
+        cv_experience = self.browser.find_element_by_id('cv_experience').text
+        cv_extracurricular_activities = self.browser.find_element_by_id('cv_extracurricular_activities').text
+        cv_skills_and_interests = self.browser.find_element_by_id('cv_skills_and_interests').text
+
     def test_can_edit_forms(self):
         #Alex goes on a site to check his CV
         #When he arrives on the home screen he clicks the briefcase icon to check his CV
@@ -20,18 +27,12 @@ class NewVisitorTest(unittest.TestCase):
         print(header_text)
         self.assertIn('CV',header_text)
         sub_header_text = self.browser.find_elements_by_tag_name('h3')
-        check_for_forms = self.browser.find_element_by_id('cv_profile').text
-        form_list = self.browser.find_elements_by_tag_name('textfield')
+        self.check_if_all_forms_present()
         sub_header_check_list =["Profile","Education","Experience","Extra-curricular Achievements","Skills and Interests"]
         for i in range(len(sub_header_text)):
             self.assertEqual(sub_header_text[i].text,sub_header_check_list[i])
-        print("!!!!!!!!!!!!!!!!!!")
-        print(check_for_forms)
-        print("!!!!!!!!!!!!!!!!!!")
-        for form in form_list:
-            print("it works")
-            form.send_keys("test!")
-            self.assertEqual(form.text,"test!")
+        #There are 5 forms
+        
         #He clicks on the edit button to edit the CV
 
         #He clicks on the save button to save the new CV
